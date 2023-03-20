00725dfafa2b527c127251b9b974a0aa65b401e2
afermor8 <ara.fer.mor@gmail.com>
2023-03-20 11:28:46 +0000

    He hecho cambios en servidor de producción

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '9f0h)gozf$g%6igo8&767w1xro0adm+)msxe)!eic$!fhvynb8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['python.afm-tars.es','217.160.225.205']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'USER': 'arantxa',
+        'PASSWORD': 'Tequiero8*',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
